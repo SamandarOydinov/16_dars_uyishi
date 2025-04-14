8abe5ea79d0ebed72b7204982cacba1e19a78766
SamandarOydinov <samandaroydinov0001@gmail.com>
2025-04-14 18:08:01 +0500

    3 - commit

--- src/app/Counter/index.jsx
+++ src/app/Counter/index.jsx
@@ -3,7 +3,7 @@ import React from "react"
 function Counter() {
     return (
      <div>
-        <h1> My Counter !</h1>
+        <h1> My Counter   Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolorum nesciunt ut possimus unde rerum odit. Non repudiandae eum dolorum omnis natus debitis, totam, impedit nam voluptate reprehenderit earum quibusdam?!</h1>
      </div>
     )
   }
